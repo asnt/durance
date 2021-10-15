18378c327aa97939ff35124fc805cb71ca2b76f2
Alexandre Saint <snt.alex@gmail.com>
2021-10-15 19:38:12 +0200

    Format

--- analyse_hrv.py
+++ analyse_hrv.py
@@ -146,9 +146,10 @@ def compute_dfa(pp, scale_min=16, scale_max=32, n_scales_max=None):
         # First order polynomial
         A = np.hstack((A0, ones))
         # Second order polynomial
-        # TODO: Detrending with second-order polynomial. Seems to work with
-        # longer window size and larger maximum scale (i.e. len(rr) / 4).
-        # A = np.hstack((A0 ** 2, A0, ones))
+        # TODO: Check how this compares to a first-order polynomial. Seems to
+        # work with longer window size and larger maximum scale
+        # (i.e. len(rr) / 4).
+        A = np.hstack((A0 ** 2, A0, ones))
         B = y.T
         x, residuals, rank, singular = np.linalg.lstsq(A, B, rcond=None)
 
