5e804bb2127c7f349dcc1386a4507377ce896026
Alexandre Saint <snt.alex@gmail.com>
2021-10-25 23:29:31 +0200

    Use html template

--- webapp/__main__.py
+++ webapp/__main__.py
@@ -10,7 +10,7 @@ class Activities:
     def __init__(self, db="activities.db"):
         self.db = activity.db_connect(db)
 
-    def list(self):
+    def values(self):
         cursor = self.db.cursor()
         query = "select * from activities"
         output = cursor.execute(query)
@@ -20,13 +20,9 @@ class Activities:
 @app.route("/", methods=["GET"])
 def index():
     activities = Activities()
-    items = activities.list()
-    activity_list_html = "\n".join(
-        str(item) + r"<br/>"
-        for item in items
-    )
-    html = f"""<html><body>{activity_list_html}</body></html>"""
-    return html
+    activities_values = activities.values()
+    return render_template("activities.html",
+                           activities_values=activities_values)
 
 
 if __name__ == '__main__':
