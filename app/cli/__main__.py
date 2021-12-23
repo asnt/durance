ec5b7272c29af44a32e22898acac10e7b54c3ca9
Alexandre Saint <snt.alex@gmail.com>
2021-12-23 15:52:39 +0100

    Report number of newly imported activities

--- app/cli/__main__.py
+++ app/cli/__main__.py
@@ -19,22 +19,44 @@ def init_db(args) -> None:
 
 
 def cmd_import_activities(args) -> None:
-    import_activities(args.files)
+    new = import_activities(args.files)
+    logger.info(f"imported {new}/{len(args.files)} new activities")
 
 
-def import_activities(paths: Sequence[os.PathLike]) -> None:
+def import_activities(paths: Sequence[os.PathLike]) -> int:
+    """Import a series of activities, if not already imported.
+
+    Returns:
+        Number of activities imported.
+    """
     logger.debug(f"importing {len(paths)} activities")
+    n_imports = 0
     for path in paths:
-        import_activity(path)
+        new = import_activity_if_new(path)
+        if new:
+            n_imports += 1
+    return n_imports
 
 
-def import_activity(path: os.PathLike) -> None:
+def import_activity_if_new(path: os.PathLike) -> bool:
+    """Import an activity, if not already imported.
+
+    Returns:
+        `True` upon import, `False` if already imported.
+    """
     path = pathlib.Path(path)
-    logger.debug(f"importing {path}")
 
     if app.model.has_activity(path):
         logger.debug("activity already imported")
-        return
+        return False
+
+    import_activity(path)
+    return True
+
+
+def import_activity(path: os.PathLike):
+    path = pathlib.Path(path)
+    logger.debug(f"importing {path}")
 
     activity_data, recordings_data = hrv.data.load(path)
     activity_data["file_hash"] = app.model.hash_file(path)
