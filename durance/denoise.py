64864492be9d5bdb45040862d463bb125a646fc2
Alexandre Saint <snt.alex@gmail.com>
2022-02-05 15:25:06 +0100

    Document

--- durance/denoise.py
+++ durance/denoise.py
@@ -1,78 +1,82 @@
 import numpy as np
 
 
-def find_inliers(rr: np.ndarray, method: str = "moving_median") -> np.ndarray:
-    """Find valid RR signal samples.
+def find_inliers(
+    signal: np.ndarray,
+    method: str = "moving_median",
+) -> np.ndarray:
+    """Find samples deviating significantly from main trend of signal.
+
+    Can filter noisy signal of heartbeat intervals (RR) into clean signal (NN).
+    For example, filter out missed heartbeats.
 
     Parameters
     ----------
-    rr:
-        Raw signal of RR intervals. Shape (n,).
-    method: {"moving_median", "deviation", "deviation_forward"}
+    signal : float[n]
+        Signal with spurious samples.
+    method : {"moving_median", "deviation", "deviation_forward"}
         Method to identify outliers.
 
     Returns
     -------
-    Boolen mask of valid samples. Shape (n,).
+    bool[n]
+        Mask of valid samples.
     """
     if method == "deviation":
-        mask_valid = inliers_from_deviation(rr)
+        mask_valid = inliers_from_deviation(signal)
     elif method == "deviation_forward":
-        mask_valid = inliers_from_deviation_forward(rr)
+        mask_valid = inliers_from_deviation_forward(signal)
     elif method == "moving_median":
-        mask_valid = inliers_from_moving_median(rr)
+        mask_valid = inliers_from_moving_median(signal)
     elif method == "wavelet":
         raise NotImplementedError
         # XXX: Does not work. Loss of details?
-        mask_valid = np.full_like(rr, True)
+        mask_valid = np.full_like(signal, True)
     else:
         raise ValueError(f"invalid method {method}")
     return mask_valid
 
 
-def inliers_from_deviation_forward(rr):
-    """Find valid RR signal samples from the deviation to the next sample.
+def inliers_from_deviation_forward(signal: np.ndarray) -> np.ndarray:
+    """Find valid samples in signal from forward deviation.
 
     Parameters
     ----------
-    rr: array-like
-        (n,) signal of RR intervals.
+    signal : float[n]
+        Signal with spurious samples.
 
     Returns
     -------
-    mask_valid: array-like
-        (n,) boolen mask array of the valid samples.
+    bool[n]
+        Mask of valid samples.
     """
-    diff_0 = np.diff(rr)
+    diff_0 = np.diff(signal)
     diff_0 = np.concatenate((diff_0, [np.inf]))
-    relative_variation_0 = np.abs(diff_0) / np.abs(rr)
+    relative_variation_0 = np.abs(diff_0) / np.abs(signal)
     threshold_variation = 0.05
     mask_valid = relative_variation_0 < threshold_variation
     return mask_valid
 
 
-def inliers_from_deviation(rr):
-    """Find valid RR signal samples from sample-to-sample deviation.
-
-    The deviations from both the previous and the next samples must be within
-    the threshold.
+def inliers_from_deviation(signal: np.ndarray) -> np.ndarray:
+    """Find valid samples in signal from backward and forward deviations.
 
     Parameters
     ----------
-    rr: array-like
-        (n,) signal of RR intervals.
+    signal : float[n]
+        Signal with spurious samples.
 
     Returns
     -------
-    mask_valid: array-like
-        (n,) boolen mask array of the valid samples.
+    bool[n]
+        Mask of valid samples.
     """
-    diff_0 = np.diff(rr)
-    diff_1 = np.diff(rr[::-1])[::-1]
+    diff_0 = np.diff(signal)
+    diff_1 = np.diff(signal[::-1])[::-1]
     diff_0 = np.concatenate((diff_0, [0]))
     diff_1 = np.concatenate(([0], diff_1))
-    relative_variation_0 = np.abs(diff_0) / np.abs(rr)
-    relative_variation_1 = np.abs(diff_1) / np.abs(rr)
+    relative_variation_0 = np.abs(diff_0) / np.abs(signal)
+    relative_variation_1 = np.abs(diff_1) / np.abs(signal)
     threshold_variation = 0.05
     # threshold_variation = 0.10
     mask_valid = (
@@ -83,49 +87,50 @@ def inliers_from_deviation(rr):
 
 
 def inliers_from_moving_median(
-    rr: np.ndarray,
+    signal: np.ndarray,
     window_size: int = 31,
     method: str = "absolute",
     threshold: float = 0.015,
 ) -> np.ndarray:
-    """Find valid samples in an RR signal using a moving median.
+    """Find valid samples in signal from moving median.
 
     Parameters
     ----------
-    rr: array-like
-        (n,) signal of RR intervals.
-    window_size: int
-        Size of the moving window.
-    method: {"absolute", "quantile"}
+    signal : float[n]
+        Signal with suprious samples.
+    window_size :
+        Size of moving window.
+    method : {"absolute", "quantile"}
         Method to determine inliers.
-    threshold: float
-        Maxixum distance from the moving median for an inlier. Maximum
-        deviation in seconds for method 'absolute' (e.g. 0.015). Quantile value
-        (unitless) in (0, 1) for method 'quantile' (e.g. 0.85).
+    threshold :
+        Maxixum distance from moving median for an inlier.
+        Maximum deviation in seconds for method 'absolute' (e.g. 0.015).
+        Unitless quantile value in (0, 1) for method 'quantile' (e.g. 0.85).
 
     Returns
     -------
-    mask_valid: array-like
-        (n,) boolen mask array of the valid samples.
+    bool[n]
+        Mask of valid samples.
     """
     assert method in ("absolute", "quantile")
     if method == "quantile":
-        assert 0 < threshold < 1.0
+        assert 0.0 < threshold < 1.0
 
     pad_before = window_size // 2
     pad_after = window_size - pad_before - 1
     pad_widths = pad_before, pad_after
-    rr_padded = np.pad(rr, pad_widths,
-                       mode="constant", constant_values=np.nan)
+    signal_padded = np.pad(
+        signal, pad_widths, mode="constant", constant_values=np.nan,
+    )
 
     sliding_window_view = np.lib.stride_tricks.sliding_window_view
-    windows = sliding_window_view(rr_padded, window_size)
+    windows = sliding_window_view(signal_padded, window_size)
     # Need explicit cast to dtype float for some operations below (e.g.
     # np.nanmedian). By default, the sliding window views have dtype object.
     windows = windows.astype(float)
 
     medians = np.nanmedian(windows, axis=1)
-    deviations = np.abs(rr - medians)
+    deviations = np.abs(signal - medians)
 
     if method == "absolute":
         threshold_ = threshold
@@ -137,25 +142,39 @@ def inliers_from_moving_median(
     return mask_valid
 
 
-def inliers_from_swt(rr):
+def inliers_from_swt(signal: np.ndarray) -> np.ndarray:
+    """Find valid samples in signal from stationary wavelet transform.
+
+    Parameters
+    ----------
+    signal : float[n]
+        Signal with spurious samples.
+
+    Returns
+    -------
+    bool[n]
+        Mask of valid samples.
+    """
+    # FIXME: Experimental. Does not work yet.
+
     import pywt
     wavelet = pywt.Wavelet("db9")
-    rr_valid = np.copy(rr)
+    signal_valid = np.copy(signal)
 
-    power2_length = 1 + int(np.ceil(np.log2(len(rr_valid))))
+    power2_length = 1 + int(np.ceil(np.log2(len(signal_valid))))
     length_padded = 2 ** power2_length
-    pad_width = length_padded - len(rr_valid)
+    pad_width = length_padded - len(signal_valid)
     pad_before = pad_width // 2
     pad_after = pad_width - pad_before
     # mode = "constant"
     mode = "symmetric"
-    rr_padded = pywt.pad(rr_valid, (pad_before, pad_after), mode)
+    signal_padded = pywt.pad(signal_valid, (pad_before, pad_after), mode)
     mask_padding = np.full(length_padded, np.nan)
     mask_padding[pad_before:length_padded - pad_after] = 1
 
     n_levels = 8
     normalise = True
-    coef = pywt.swt(rr_padded, wavelet, level=n_levels, norm=normalise)
+    coef = pywt.swt(signal_padded, wavelet, level=n_levels, norm=normalise)
 
     def threshold_over(x, threshold=1):
         mask_over_threshold = np.abs(x) > threshold
@@ -174,8 +193,8 @@ def inliers_from_swt(rr):
         ca, cd = coef_thresholded[level]
         ca_zeroed = np.zeros_like(ca)
         coef_thresholded[level] = ca_zeroed, cd
-    rr_denoised = pywt.iswt(coef_thresholded, wavelet, norm=normalise)
-    rr_denoised = rr_denoised[pad_before:length_padded - pad_after]
-    assert len(rr_denoised) == len(rr_valid)
+    signal_denoised = pywt.iswt(coef_thresholded, wavelet, norm=normalise)
+    signal_denoised = signal_denoised[pad_before:length_padded - pad_after]
+    assert len(signal_denoised) == len(signal_valid)
 
-    return rr_denoised
+    return signal_denoised
